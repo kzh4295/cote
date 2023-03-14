fae2e6dcf6578f33a00bbbab2002f2e553efaae5
jihye kim <74666269+kzh4295@users.noreply.github.com>
2023-03-14 09:39:11 +0900

    [1주차] 김지혜 풀이 등록

--- 1_week/김지혜/solution.js
+++ 1_week/김지혜/solution.js
@@ -1,1 +1,19 @@
+function solution(ingredient){    
+    let answer = 0;    
+    let str = ingredient.join('');    
+    let idx = str.indexOf('1231');        
+    console.log(idx)
+    if(idx === 0){
+        console.log("str-0-1:", str)
+        str = str.slice(0, idx) + str.slice(idx+5)
+        console.log("str-0-2:", str)
+        answer++        
+    } else if(idx > 0){
+        console.log("str-1-1:", str)
+        str = str.slice(0, idx) + str.slice(idx+5)
+        console.log("str-1-2:", str)
+        answer++
+    } 
+  return answer    
+}
 
