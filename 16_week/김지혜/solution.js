9cb3114f3bb7b4aeb3c9eee943bebdfb2dd763f2
jihye kim <74666269+kzh4295@users.noreply.github.com>
2023-06-20 19:34:25 +0900

    [16주차] 김지혜 풀이 수정

--- 16_week/김지혜/solution.js
+++ 16_week/김지혜/solution.js
@@ -2,38 +2,23 @@
 function solution(n, t, m, p) {
   let answer = '';
   
-  // tempArray라는 변수에 돌릴 변수 틀 만들고 넣기
-  // 배열 요소 개수(t - 1) * m + p 먼저 틀 만들고
-  let tempArray =[];
-  // 배열의 요소들을 진수 변환해서 PUSH로 넣기
-  for(let i=0; i <(t - 1) * m + p; i++){
-    tempArray.push(i.toString(n))
+  // 게임 회차별로 진수에 맞게 변환되서 내야할 숫자 범위(참가할 인원 * 튜브의 순서) 구하기
+  let gameCount ='';
+  for(let i=0; i < m * t; i++){
+    gameCount += i.toString(n)
   }
 
-   // reduce를 이용하여 1~m명까지 한명씩 차례대로 배열 만들기
-  tempArray = [...tempArray.join().replace(/,/g, "")]
-  // 배열의 길이 제한
-  tempArray.length=m*t;
+  // 게임 회차에 따라 참여 인원이 내야할 값을 배열로 분류하기
+  gameValueArray = [...gameCount]
 
-  // player들이 순서대로 낼 값을 키-값 형태로 저장
-  let player = Array.from({length:m},(v, i)=>i+1)
-  const result = tempArray.reduce((acc, curr, index) => {
-    const currentPlayer = player[index % player.length];
-    acc[currentPlayer]+=curr;
+  // 튜브가 내야할 값만 tubeValue에 담기
+  const tubeValue = gameValueArray.reduce((acc, cur, index) => {
+      // 나머지 값이 p값과 같으면(튜브의 순서값만) 누적하기
+    if((index % m+1) === p) acc += cur;
     return acc;
-  },{});
-  
-  // player 키값과 p값이 일치하는 i값의 밸류를 구하기
-  let playerLength = Object.keys(result).length;
-  for(let i = 0; i<playerLength; i++){
-    if(Object.keys(result)[i]*1 === p) {
-      answer = Object.values(result)[i]
-      break;
-    }
-    else answer = ''
+  },'');
     
-  }
-
-  // 앞의 undefined를 제거하고 소문자를 대문자로 변환하여 answer값 구함
-  return answer.replace('undefined', '').toUpperCase();
+  // result에서 t길이 만큼의 값을 구하고 대문자로 변환한 값을 answer에 할당하기
+  answer = tubeValue.slice(0, t).toUpperCase();
+  return answer
 }
